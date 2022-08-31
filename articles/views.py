e1948047b0c2b4fb529524ec52ba8321fc9340de
Ashwini Surve <ashwini.mca.89@gmail.com>
2022-08-31 15:25:09 -0400

    django blogs

---
+++ articles/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .models import Article
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from . import forms
+
+# Create your views here.
+@login_required()
+def article_list(request):
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {
+    'articles': articles
+    })
+
+@login_required()
+def article_detail(request, slug):
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {
+    'article': article
+    })
+
+@login_required()
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('articles:list')
+    else:
+        create_form = forms.CreateArticle()
+        return render(request, 'articles/article_create.html', {'form': create_form })
